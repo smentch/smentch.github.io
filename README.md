7da78f62f95e5b8dcbff20ea4e24fd8ad8899f75
James Huffaker <james.huffaker@gmail.com>
2025-10-27 16:54:53 -0700

    Initial page creation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # smentch.github.io
+
+bundle exec jekyll serve
---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+(function() {
+    window.addEventListener('load', function() {
+        setTimeout(function() {
+          document.body.classList.remove('is-preload');
+        }, 100);
+      });
+})();
